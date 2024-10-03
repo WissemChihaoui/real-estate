6746d641d0c7222bdbce6d73f5e5778087f6b3e0
ghassensta <staghassen1@gmail.com>
2024-10-03 23:27:20 +0100

    real estate

---
+++ src/components/singleCard/SingleCard.jsx
@@ -0,0 +1,24 @@
+import './SingleCard.scss'
+const SingleCard = () => {
+  return (
+    <div className='singleCard'>
+        <img src="https://images.pexels.com/photos/276724/pexels-photo-276724.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <div className="info">
+            <h2>Card Title</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel nunc nec arcu finibus faucibus.</p>
+            <div className="features">
+                <div className="feature">
+                    <img src="/bed.png" alt="" />
+                    <span>1 bedroom</span>
+                </div>
+                <div className="feature">
+                    <img src="/bath.png" alt="" />
+                    <span>1 bathroom</span>
+                </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default SingleCard
